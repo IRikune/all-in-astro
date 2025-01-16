f04c2e7c45c448e4cb3474982464a72c3c7b328c
CodeArmer <geanmotas@gmail.com>
2025-01-16 18:33:11 -0400

    added getNotes function to get all notes from DB

--- 2-todos/deno/models/notes.ts
+++ 2-todos/deno/models/notes.ts
@@ -1,1 +1,10 @@
+import type { Note } from "../types/mod.ts";
 import { kv } from "../main.ts";
+
+export async function getNotes({ id = "public" }) {
+  const key = ["notes", id];
+  const iter = kv.list<Note>({ prefix: key });
+  const notes: Note[] = [];
+  for await (const note of iter) notes.push(note.value);
+  return notes;
+}
