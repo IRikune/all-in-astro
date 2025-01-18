c27ecde13bcb3bf98f4d4365de6caf136da95a18
CodeArmer <geanmotas@gmail.com>
2025-01-18 17:01:48 -0400

    Added endpoint to get notes from a user

--- 2-todos/deno/routes/notes.ts
+++ 2-todos/deno/routes/notes.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import { getNotes } from "../models/notes.ts";
 
 export const notes = new Hono();
 
-notes.get("/", (c) => {
-  return c.text("notes");
+notes.get("/notes/:userID?", async (c) => {
+  const userID = c.req.param("userID");
+  const notes = await getNotes({ userID });
+  const result = { ok: true, data: notes };
+  return c.json(result);
 });
