9023abdc1299a51f609a16e172dc4cee8ea620c4
CodeArmer <geanmotas@gmail.com>
2025-01-26 17:49:17 -0400

    Added Avatar Component

---
+++ 2-todos/src/components/preact/Avatar.tsx
@@ -0,0 +1,20 @@
+import { useMemo } from "preact/hooks";
+import { createAvatar } from "@dicebear/core";
+import { avataaars as avatars } from "@dicebear/collection";
+
+interface AvatarProps {
+    seed?: string;
+    size?: number;
+    class?: string;
+}
+export function Avatar({ size = 128, seed = "riku", class: className }: AvatarProps) {
+    const avatar = useMemo(() => {
+        const avatarOptions = {
+            seed,
+            size,
+        }
+        const avatarSrc = createAvatar(avatars, avatarOptions).toDataUri()
+        return avatarSrc
+    }, [])
+    return <img class={`${className}`} src={avatar} alt="Avatar" />
+}
