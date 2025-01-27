5d1d1bfea7e4ff9aa3c6411d9d46a2678f695f53
CodeArmer <geanmotas@gmail.com>
2025-01-27 15:20:14 -0400

    Updated Biome linter rules

--- 2-todos/src/components/preact/icons/Icon.tsx
+++ 2-todos/src/components/preact/icons/Icon.tsx
@@ -1,18 +1,21 @@
-import type { Icon } from "./mod"
+import type { Icon as IconType } from "./mod"
 import { icons } from "./mod"
 
 interface IconProps {
-    name: Icon["name"]
+    name: IconType["name"]
     class?: string
 }
 export function Icon({ name, class: className }: IconProps) {
     const displayIcon = icons.find(icon => icon.name === name)
     return (
         <svg
+            // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+            // biome-ignore lint/security/noDangerouslySetInnerHtmlWithChildren: <explanation>
             dangerouslySetInnerHTML={{ __html: displayIcon?.svg || '' }}
             class={className}
             viewBox='0 0 24 24'
         >
+            <title>{name}</title>
         </svg>
     )
 }
