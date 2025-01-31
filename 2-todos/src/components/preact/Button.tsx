62f5322af16980447bfc67604c0c3a1cdba3937e
CodeArmer <geanmotas@gmail.com>
2025-01-31 14:22:42 -0400

    Updated Button component styles

--- 2-todos/src/components/preact/Button.tsx
+++ 2-todos/src/components/preact/Button.tsx
@@ -13,13 +13,13 @@ interface Props {
 export function Button({ type = "underline", to, children = "Button", class: className, icon }: Props) {
     const Tag = to ? "a" : "button";
     const styles = {
-        underline: 'relative px-2 before:content-[""] before:absolute before:-bottom-1 before:left-0 before:w-0 before:h-[2px] before:rounded-full before:duration-300 before:transition-all before:bg-black hover:before:w-full',
+        standard: ["flex w-fit items-center text-sm", "w-6 fill-transparent stroke-white mr-2"],
         filled: 'text-white border-transparent border-2 rounded bg-black px-4 py-2 hover:bg-transparent hover:border-black transition-all duration-300 hover:text-black',
         outlined: 'border-black border-2 rounded bg-transparent px-4 py-2 hover:bg-black transition-all duration-300 hover:text-white',
     }
     return (
-        <Tag href={to} className={`flex w-fit ${className}`}>
-            <Icon class="w-6 fill-transparent stroke-neutral-500" name={icon} />
+        <Tag href={to} className={`flex bg-orange-200 px-2 py-1 rounded w-fit items-center text-sm text-orange-700 ${className}`}>
+            <Icon class="w-6 fill-orange-600 stroke-orange-200 mr-4" name={icon} />
             {children}
         </Tag>
     )
