644e28385934a1a40a36a3ea9da926263015fd97
nander1117 <jhonander1117@gmail.com>
2025-02-19 14:12:28 -0400

    add DropDown

--- 2-todos/src/components/preact/DropDown.tsx
+++ 2-todos/src/components/preact/DropDown.tsx
@@ -1,31 +1,22 @@
-import { useState } from "preact/hooks";
 import { Button } from "./Button";
 import { Icon } from "./icons/Icon";
 interface Props {
     class: string;
-    children:string;
 }
-export function DropDown({ children, class: className }: Props) {
-    const [isOpen, setIsOpen] = useState(false);
-    let useClickOutside = () => setIsOpen(!isOpen);
+export function DropDown({ class: className }: Props) {
+   
     return (
             <>
-                <button class={`h-4 w-4 p-0 relative ${className} `} onMouseOver={useClickOutside} onMouseOut={useClickOutside}>
-                    {children}
-                    
-                    <Icon name="bell" class="w-full h-full "></Icon>
-                    
-                    {
-                        isOpen && <Drops/>
-                    }
-                    
-                </button>
+                <Button class={` p-0 relative justify-between *:first: *:last:invisible focus:*:last:visible ${className}`}  >
+                    <Drops/>
+
+                </Button>
             </>
     )   
 } 
 function Drops(){
     return <>
-        <div  class={"absolute z-10 w-24 h-24 border-2 bg-amber-50 rounded-md "}>
+        <div  class={"absolute block p-1.5 top-10 z-10 w-96 h-96 overflow-ellipsis border-2 bg-white rounded-md "}>
             <ul>
                 <li>hola       </li>
                 <li>adios      </li>
