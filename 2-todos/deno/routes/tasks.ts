a3e679c42740c4cb084122b95a2a5fa70f799e71
CodeArmer <geanmotas@gmail.com>
2025-02-22 16:55:50 -0400

    Updated types, schemas and added post task method

--- 2-todos/deno/routes/tasks.ts
+++ 2-todos/deno/routes/tasks.ts
@@ -1,5 +1,8 @@
 import { Hono } from "hono";
-import { getTasks } from "../models/tasks.ts";
+import { validator } from "hono/validator";
+import { HTTPException } from "hono/http-exception";
+import { postTaskSchema, taskSchema, userIDSchema } from "../schemas/tasks.ts";
+import { createTask, getTasks } from "../models/tasks.ts";
 
 export const tasks = new Hono();
 
@@ -9,3 +12,27 @@ tasks.get("/tasks/:userID?", async (c) => {
   const result = { ok: true, data: tasks };
   return c.json(result);
 });
+
+tasks.post(
+  "/tasks/:userID",
+  validator("json", (value) => {
+    const parsed = postTaskSchema.safeParse(value);
+    if (!parsed.success) {
+      throw new HTTPException(400, parsed.error);
+    }
+    return parsed.data;
+  }),
+  validator("param", (value) => {
+    const parsed = userIDSchema.safeParse(value);
+    if (!parsed.success) {
+      throw new HTTPException(400, parsed.error);
+    }
+    return parsed.data;
+  }),
+  async (c) => {
+    const userID = c.req.valid("param");
+    const task = c.req.valid("json");
+    const result = await createTask({ userID, task });
+    return c.json(result);
+  },
+);
--- 2-todos/deno/schemas/tasks.ts
+++ 2-todos/deno/schemas/tasks.ts
@@ -1,39 +1,35 @@
 import { z } from "zod";
 
-const commentSchema = z.object({
+export const userIDSchema = z.string().ulid();
+
+export const commentSchema = z.object({
   id: z.string().ulid().optional(),
   creator: z.string().ulid(),
   content: z.string(),
   createdAt: z.number(),
 });
 
-export const subTaskSchema = z.object({
+const baseSchema = z.object({
   id: z.string().ulid().optional(),
-  creator: z.string().ulid(),
   title: z.string(),
-  content: z.string(),
+  creator: userIDSchema,
+  content: z.string().optional(),
   completed: z.boolean(),
   comments: z.array(commentSchema).optional(),
   date: z.object({
     created: z.number(),
-    completed: z.number(),
-    expected: z.number(),
+    completed: z.number().optional(),
+    expected: z.number().optional(),
   }),
   priority: z.number().min(1).max(4),
 });
 
-export const taskSchema = z.object({
-  id: z.string().ulid().optional(),
-  creator: z.string().ulid(),
-  title: z.string(),
-  content: z.string(),
-  completed: z.boolean(),
-  comments: z.array(commentSchema),
+export const subTaskSchema = baseSchema.extend({});
+
+export const taskSchema = baseSchema.extend({
   subTasks: subTaskSchema.array().optional(),
-  date: z.object({
-    created: z.number(),
-    completed: z.number(),
-    expected: z.number(),
-  }),
-  priority: z.number().min(1).max(4),
+});
+
+export const postTaskSchema = taskSchema.extend({
+  id: z.never().optional(),
 });
