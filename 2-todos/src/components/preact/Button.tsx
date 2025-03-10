5fd9863ea43c9f5ceae7bd9a9b189cd6691c3f13
nander1117 <jhonander1117@gmail.com>
2025-03-10 16:22:54 -0400

    | update Button |

--- 2-todos/src/components/preact/Button.tsx
+++ 2-todos/src/components/preact/Button.tsx
@@ -9,6 +9,7 @@ interface Props {
     children?: ComponentChildren;
     class?: string;
     onclick?:()=>void
+    color?:string
 }
 
 
@@ -16,7 +17,7 @@ export function Button({ to, page, children, class: className, icon, onclick }: 
     const Tag = to ? "a" : "button";
     return (
         <Tag href={to} className={`${className} hover:bg-gray-200 flex transition-colors duration-200 p-0 *:m-0 rounded text-sm text-neutral-700 tracking-tight cursor-pointer  items-center
-        ${to != null && to === page && "bg-rose-100"}  `} onClick={onclick}>
+        ${to != null && to === page && "bg-rose-100"}  `} onClick={onclick} >
             {
                 icon &&
                 <Icon class="w-5 fill-transparent stroke-neutral-500 mr-2" name={icon} />
