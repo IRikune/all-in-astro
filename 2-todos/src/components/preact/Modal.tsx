748e50f8fa06831484ad91ae96a655cad9cc08a6
CodeArmer <geanmotas@gmail.com>
2025-04-16 17:51:21 -0400

    Updated Modal component

--- 2-todos/src/components/preact/Modal.tsx
+++ 2-todos/src/components/preact/Modal.tsx
@@ -7,7 +7,7 @@ interface ModalProps extends JSX.HTMLAttributes<HTMLDivElement> {
 
 export function Modal(props: ModalProps) {
     return (
-        <div class="has-checked:*:block has-not-checked:*:hidden has-checked-:*:transition-[opacity_display] *:transition-discrete *:duration-500 has-not-checked:duration-500 peer-not-checked:opacity-0">
+        <div class="has-checked:*:block has-not-checked:*:hidden has-checked-:*:transition-[opacity_display] *:transition-discrete *:duration-300 has-not-checked:duration-300 peer-not-checked:opacity-0">
             {/* State */}
             <input type="checkbox" id={props.id} class="peer hidden!" />
             <label for={props.id}>
