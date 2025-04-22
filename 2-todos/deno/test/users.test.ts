82a60774eae8e5bf27e727bec45c8cb6b356579e
CodeArmer <geanmotas@gmail.com>
2025-04-22 18:53:16 -0400

    Added all users tests

--- 2-todos/deno/test/users.test.ts
+++ 2-todos/deno/test/users.test.ts
@@ -1,15 +1,18 @@
-import { assertEquals, assertObjectMatch } from "https://deno.land/std@0.150.0/testing/asserts.ts"
-import type { NewUser } from "../types/mod.ts";
+import { assertObjectMatch } from "https://deno.land/std@0.150.0/testing/asserts.ts"
+import type { NewUser, User } from "../types/mod.ts";
 
 const ENDPOINT = Deno.env.get("ENDPOINT") || "http://localhost:8000/"
 
-Deno.test("Should the user endpoint return a response with user data", async () => {
-    const user = {
-        name: "Testing",
-        email: "testing@test.com",
-        password: "test.test",
-        avatar: "https://astro.build/assets/logo.svg"
-    } as NewUser
+const user = {
+    name: "Testing",
+    email: "testing@test.com",
+    password: "test.test",
+    avatar: "https://astro.build/assets/logo.svg"
+} as NewUser
+
+let createdUser = {} as User
+
+Deno.test("Should the user create endpoint return a response with user data", async () => {
 
     const res = await fetch(`${ENDPOINT}users/`, {
         method: "POST",
@@ -20,6 +23,7 @@ Deno.test("Should the user endpoint return a response with user data", async () 
     })
 
     const data = await res.json()
+    createdUser = {...data}
     const expectedResponse = {
         ok: true,
         data: {
@@ -29,12 +33,57 @@ Deno.test("Should the user endpoint return a response with user data", async () 
             password: user.password,
             avatar: user.avatar,
         },
-        timestamp: data.timestamp
+        message: "User created",
+        timestamp: data.timestamp,
     }
     // remove the user
     await fetch(`${ENDPOINT}users/${data.id}/`, {
         method: "DELETE"
     })
+    assertObjectMatch(expectedResponse, data)
+})
+
+Deno.test("Should the user delete endpoint return a response with user deleted data", async () => {
+    const res = await fetch(`${ENDPOINT}users/${createdUser.id}/`, {
+        method: "DELETE"
+    })
+    const data = await res.json()
+    const expectedResponse = {
+        ok: true,
+        data: {
+            id: createdUser.id,
+        },
+        message: "User deleted"
+    }
+    assertObjectMatch(expectedResponse, data)
+})
+
+Deno.test("Should the user update endpoint return a response with user updated data", async () => {
+    const newUser = {
+        name: "Test",
+        email: "test2@test.com",
+        password: "test.test",
+        avatar: "https://astro.build/assets/logo.svg"
+    } as NewUser
+    const res = await fetch(`${ENDPOINT}users/${createdUser.id}/`, {
+        method: "PATCH",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
+    const expectedResponse = {
+        ok: true,
+        data: {
+            id: createdUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password,
+            avatar: newUser.avatar,
+        },
+        message: "User updated"
+    }
     assertObjectMatch(expectedResponse, data)
 })
 
